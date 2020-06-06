35f25fcb535d0d13d2a18392f3dae33b351541d4
gonza-lab <gonzaflores358@gmail.com>
2020-06-06 20:22:34 -0300

    nada

--- app/src/main/java/com/example/wessons/MainActivity.java
+++ app/src/main/java/com/example/wessons/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     public void RegistrarInstitucion(View view){
         Intent registrar = new Intent(this, RegistrarInstitucionActivity.class);
         startActivity(registrar);
+        
     }
 }

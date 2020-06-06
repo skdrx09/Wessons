bf32258b26701e165d0ab67b07ef760ff0be10a2
gonza-lab <gonzaflores358@gmail.com>
2020-06-06 20:19:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/wessons/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.wessons;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Pasar de la pantalla principal a la pantalla Registrar Institucion
+    public void RegistrarInstitucion(View view){
+        Intent registrar = new Intent(this, RegistrarInstitucionActivity.class);
+        startActivity(registrar);
+    }
+}
